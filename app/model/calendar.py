4a24d81e67d8a8fe561e087e2840a8ac1c3f48df
Mafe <maguirre065@soyudemedellin.edu.co>
2024-09-12 13:52:31 -0500

    class Evemt

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -16,3 +16,26 @@ class Reminder:
     def __str__(self) ->str:
         return f"Reminder on {self.date_time} of type {type}"
 
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: datetime.date
+    start_at: datetime.time
+    end_at: datetime.time
+    id: str = field(default_factory=generate_unique_id)
+    reminders: list['Reminder'] = field(default_factory=list)
+
+    def __str__(self) -> str:
+        return f"ID: {self.id}\nEvent title: {self.title}\nDescription: {self.description}\nTime: {self.start_at} - {self.end_at}"
+
+    def add_reminder(self, date_time: datetime, type_: str = Reminder.EMAIL) -> None:
+        reminder = Reminder(date_time, type_)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index: int) -> None:
+        if 0 <= reminder_index < len(self.reminders):
+            self.reminders.pop(reminder_index)
+        else:
+            reminder_not_found_error()
+
